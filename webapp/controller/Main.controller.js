b7f68d4475ad675d07f18d94bfb342f05398b28e
hyun-jung-joo <wnguswjd0418@naver.com>
2025-03-09 15:10:42 +0900

    [Feat] 주석 추가

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -6,13 +6,14 @@ sap.ui.define(
         return Controller.extend("sync.c22.hw4.controller.Main", {
             onInit() {},
 
-            // 한 아이템을 선택했을 때 발생하는 이벤트에 관한 메소드 구현
+            // Task 3. 메시지 토스트로 출력
+            // 한 아이템을 선택했을 때 메시지 토스트로 정보를 출력하는 이벤트 핸들러 메소드
             onPress(oEvent) {
                 var oSelectedItem = oEvent.getSource(); // 클릭한 부분 가져오기
                 var oContext = oSelectedItem.getBindingContext("conn"); // conn 모델 부분 가져오기
                 var oData = oContext.getProperty(); // 그 안의 모든 값을 가지고 온다.
 
-                // Task 3. 메시지 토스트로 출력
+                // 메시지 토스트로 정보 출력
                 MessageToast.show(
                     `선택하신 라인은\n항공사: ${oData.CARRID}, 항공편: ${oData.CONNID} \n의 정보입니다.`,
                     {
@@ -21,22 +22,30 @@ sap.ui.define(
                 );
             },
 
+            // Task 4. Dialog 로 항공편 정보 출력
+            // 한 아이템을 선택했을 때 Dialog 로 정보를 출력하는 이벤트 핸들러 메소드
+            // 비동기로 구현해준다.
             async onOpenDialog(oEvent) {
                 var oSelectedItem = oEvent.getSource(); // 클릭한 부분 가져오기
                 var oContext = oSelectedItem.getBindingContext("conn"); // conn 모델 부분 가져오기
-                var sPath = oContext.getPath();
+                var sPath = oContext.getPath(); // 현재 선택한 부분의 경로를 가져온다.
 
+                // Dialog 를 비동기로 load 한다.
+                // 이때 보이게 할 Dialog의 경로를 적어준다.
                 this.oDialog ??= await this.loadFragment({
                     name: "sync.c22.hw4.view.Info",
                 });
 
+                // 위에서 저장해 둔 현재 아이템의 경로와 모델 정보를 함께 Dialog에 binding 해준다.
+                this.oDialog.bindElement({ path: sPath, model: "conn" });
                 // Fragment에서 가져온 Dialog를 Open
                 this.oDialog.open();
-                this.oDialog.bindElement({ path: sPath, model: "conn" });
             },
 
+            // Dialog의 닫기 버튼에 해당하는 이벤트 핸들러 메소드
             onClose() {
-                var oDialog = this.byId("idDialog");
+                var oDialog = this.byId("idDialog"); // id를 통해 Dialog 를 찾는다.
+                // 존재하다면, Dialog 를 close 해준다.
                 if (oDialog) {
                     oDialog.close();
                 }
